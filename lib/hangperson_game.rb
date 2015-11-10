a80f107be92bb47198d2ba71d27c062602c1c880
omarkhwaja <omar.khwaja@gmail.com>
2015-11-10 04:23:37 +0000

    Completed game with spec

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,17 +1,59 @@
 class HangpersonGame
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
 
-  # add the necessary class methods, attributes, etc. here
-  # to make the tests in spec/hangperson_game_spec.rb pass.
-
-  # Get a word from remote "random word" service
-
-  # def initialize()
-  # end
-  
-  def initialize(word)
-    @word = word
+  def initialize(new_word)
+    @word          = new_word.downcase
+    @guesses       = ''
+    @wrong_guesses = ''
   end
 
+  def guess(letter)
+    raise ArgumentError, 'No input'           if letter == nil
+    raise ArgumentError, 'No input character' if letter == ''
+    raise ArgumentError, 'Not a letter'       if !letter.match(/[a-zA-Z]/)
+
+    letter.downcase!
+
+    if @word.include?(letter)
+      if @guesses.include?(letter)
+        return false
+      else
+        @guesses += letter
+        return true
+      end
+    else
+      if @wrong_guesses.include?(letter)
+        return false
+      else
+        @wrong_guesses += letter
+        return true
+      end
+    end
+  end
+
+  def word_with_guesses
+    displayed_string = ''
+    @word.each_char do |ltr|
+      if @guesses.include?(ltr)
+        displayed_string += ltr
+      else
+        displayed_string += '-'
+      end
+   end
+   displayed_string
+  end
+
+  def check_win_or_lose
+    return :lose if @wrong_guesses.length >= 7
+    @word.each_char do |ltr|
+      return :play if !@guesses.include?(ltr)
+   end
+   return :win
+  end
+
+  # Get a word from remote "random word" service
   def self.get_random_word
     require 'uri'
     require 'net/http'
